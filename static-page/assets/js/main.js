929d3f5a0b1017a524f8a13a65369fcf14186c54
Raphael <raph35ral@gmail.com>
2022-08-31 09:41:18 +0300

    Initial commit and integration de la maquette

---
+++ static-page/assets/js/main.js
@@ -0,0 +1,15 @@
+const carousel = document.querySelector("#carouselDescription");
+console.log(carousel)
+const listDescription = carousel?.querySelectorAll("div[data-target='#project_carouselDescription']")
+
+console.log(listDescription);
+
+$("#project_carousel").on('slide.bs.carousel', function (e) {
+    listDescription.forEach((elt, i) => {
+        elt.classList.remove('active');
+    });
+    listDescription[e.to].classList.add('active');
+    setTimeout(() => {
+    }, 5)
+
+})
